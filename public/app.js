77c5e4ec6643e00fd85b509fb74c9af2d227942e
dbogs425 <dbolognino425@gmail.com>
2017-04-24 10:07:48 -0400

    removed class on modal button click

--- public/app.js
+++ public/app.js
@@ -3,4 +3,7 @@ $(document).ready(function () {
         $('.dashboard').hide();
         $('.main').removeClass('hide').addClass('fade');
     })
+    $('.modalbutton').on('click', function () {
+        $('.main').removeClass('fade');
+    })
 });
