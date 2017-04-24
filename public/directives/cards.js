b5ba15eb263f487e6d9a3683131559d95a188cdd
dbogs425 <dbolognino425@gmail.com>
2017-04-24 18:15:42 -0400

    directive update

--- public/directives/cards.js
+++ public/directives/cards.js
@@ -4,6 +4,7 @@ angular.module("avant")
     $scope.cards = [ {
         projectName: "Donna Drive",
         description: "",
+        coverImage: "./images/avantimages/donna/completedfireplace.JPG",
         images: ["./images/avantimages/donna/cabinets.JPG", "./images/avantimages/donna/completedfireplace.JPG", "./images/avantimages/completedkitchen.JPG", "./images/avantimages/donna/drywallhall.JPG", "./images/avantimages/donna/finishedupstairs.JPG", "./images/avantimages/donna/inprogress.JPG", "./images/avantimages/donna/tiling.JPG", "./images/avantimages/donna/upstairsdone.JPG"]
     }
     ]
