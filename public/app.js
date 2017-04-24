332a0bac0edfe9a82bcda3935d067dca821922b9
dbogs425 <dbolognino425@gmail.com>
2017-04-24 17:31:55 -0400

    some carousel stuff

--- public/app.js
+++ public/app.js
@@ -6,4 +6,32 @@ $(document).ready(function () {
     $('.modalbutton').on('click', function () {
         $('.main').removeClass('fade');
     })
+
+    $('body').scrollspy({
+        target: '#navbar-section'
+    });
+    $('#sendEmailButton').on('click', function () {
+        var mailObj = {};
+        mailObj.name = $('#name').val();
+        mailObj.number = $('#number').val();
+        mailObj.message = $('#message').val();
+
+        console.log(mailObj);
+
+        window.open("mailto:" + "dillonbolognino@avantcontracting.com" + '&subject=' + mailObj.name + " " + mailObj.number + '&body=' + mailObj.message)
+    })
+    $(".owl-carousel").owlCarousel({
+        nav: true,
+        items: 1,
+        responsive: {
+            responsiveBaseElement: document.getElementById("owlContainer"),
+            responsiveRefreshRate: 1000
+        },
+        navText: ['previous', 'next'],
+        slideBy: 1,
+        margin: 20,
+        autoWidth: false,
+        animateOut: true,
+        animateIn: true
+    })
 });
