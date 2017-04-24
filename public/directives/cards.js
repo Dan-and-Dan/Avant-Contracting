1f168264b33e1f4793451f7040db5c4487effdb1
dbogs425 <dbolognino425@gmail.com>
2017-04-24 18:22:22 -0400

    quick update to data

--- public/directives/cards.js
+++ public/directives/cards.js
@@ -3,7 +3,7 @@ angular.module("avant")
 .controller("cards", ["$scope", function($scope){
     $scope.cards = [ {
         projectName: "Donna Drive",
-        description: "",
+        description: "Home Remodeling at Donna Drive in Colonie, NY",
         coverImage: "./images/avantimages/donna/completedfireplace.JPG",
         images: ["./images/avantimages/donna/cabinets.JPG", "./images/avantimages/donna/completedfireplace.JPG", "./images/avantimages/completedkitchen.JPG", "./images/avantimages/donna/drywallhall.JPG", "./images/avantimages/donna/finishedupstairs.JPG", "./images/avantimages/donna/inprogress.JPG", "./images/avantimages/donna/tiling.JPG", "./images/avantimages/donna/upstairsdone.JPG"]
     }
