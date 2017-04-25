0b9179aad58292b0b11b130e3ba1c1fd4f9f61b7
Daniel Pak <PakJuhnMan@gmail.com>
2017-04-24 18:21:21 -0600

    Added in email functionality in angular

--- public/app.js
+++ public/app.js
@@ -1,5 +1,12 @@
 angular.module("avant" , ["avant.cards"])
 
-.controller("mainController", ["$scope", function($scope){
+.controller("mainController", ["$scope", "$window", function($scope, $window){
+    
+    $scope.sendEmail = function(email){
+        var message = email.comment; 
+        var subject = email.name + " (" + email.phone + ")";
+        $window.open("mailto:" + "dillonbolognino@avantcontracting.com" + "?subject=" + subject + "&body=" +message, "_self");
+    }
+    
     
 }]);
