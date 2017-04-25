8c93149b03585912d90922b64f27c4ed0a6d1734
Daniel Pak <PakJuhnMan@gmail.com>
2017-04-24 18:22:09 -0600

    Edited main js and added angular email functionality

--- public/main.js
+++ public/main.js
@@ -7,17 +7,4 @@ $(document).ready(function () {
         $('.main').removeClass('fade');
     })
 
-    $('body').scrollspy({
-        target: '#navbar-section'
-    });
-    $('#sendEmailButton').on('click', function () {
-        var mailObj = {};
-        mailObj.name = $('#name').val();
-        mailObj.number = $('#number').val();
-        mailObj.message = $('#message').val();
-
-        console.log(mailObj);
-
-        window.open("mailto:" + "dillonbolognino@avantcontracting.com" + '&subject=' + mailObj.name + " " + mailObj.number + '&body=' + mailObj.message)
-    })
 });
